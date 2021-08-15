86fbd90167ce85ea81826cf1df76da852cdf08ea
hyungyu Kim <ypsophila97@icloud.com>
2021-08-15 22:57:46 +0900

    [FEAT] 프로젝트 설정 및 pod install

---
+++ KakaoLoginTutorial-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KakaoLoginTutorial-iOS
+//
+//  Created by kimhyungyu on 2021/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'KakaoLoginTutorial-iOS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+pod 'KakaoSDKUser'
+  # Pods for KakaoLoginTutorial-iOS
+
+end
